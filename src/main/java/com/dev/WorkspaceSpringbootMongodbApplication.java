f8818832b85cec3fe642f572f824d4984ac1ba95
ivansimplicio <ivansimplicio@outlook.com>
2021-12-24 11:32:44 -0300

    projeto criado

---
+++ src/main/java/com/dev/WorkspaceSpringbootMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkspaceSpringbootMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkspaceSpringbootMongodbApplication.class, args);
+	}
+
+}
