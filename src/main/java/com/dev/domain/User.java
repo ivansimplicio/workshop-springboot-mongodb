4144aab58cbfd7cb45880ee11db818617648ed5a
ivansimplicio <ivansimplicio@outlook.com>
2021-12-24 19:36:15 -0300

    Conectando ao MongoDB com repository e service

--- src/main/java/com/dev/domain/User.java
+++ src/main/java/com/dev/domain/User.java
@@ -2,6 +2,9 @@ package com.dev.domain;
 
 import java.io.Serializable;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
@@ -9,11 +12,13 @@ import lombok.Setter;
 @Getter
 @Setter
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Document(collection = "users")
 public class User implements Serializable{
 	
 	public static final long serialVersionUID = 1L;
 	
 	@EqualsAndHashCode.Include
+	@Id
 	private String id;
 	private String name;
 	private String email;
