c84bf73a7a330cd120cbbfd6ffe3932a3ae9aedc
ivansimplicio <ivansimplicio@outlook.com>
2021-12-26 19:58:45 -0300

    Referenciando os posts dos usuários

--- src/main/java/com/dev/domain/User.java
+++ src/main/java/com/dev/domain/User.java
@@ -1,8 +1,11 @@
 package com.dev.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import lombok.EqualsAndHashCode;
@@ -22,6 +25,8 @@ public class User implements Serializable{
 	private String id;
 	private String name;
 	private String email;
+	@DBRef(lazy=true)
+	private List<Post> posts = new ArrayList<>();
 	
 	public User() {
 		
