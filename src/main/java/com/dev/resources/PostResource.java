342e9aeca46af03876c7804c01c35416de192887
ivansimplicio <ivansimplicio@outlook.com>
2021-12-26 21:07:39 -0300

    Consulta simples com @Query

--- src/main/java/com/dev/resources/PostResource.java
+++ src/main/java/com/dev/resources/PostResource.java
@@ -29,7 +29,6 @@ public class PostResource {
 	
 	@GetMapping(value = "/titlesearch")
 	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue="") String text){
-		
 		text = URL.decodeParam(text);
 		List<Post> list = service.findByTitle(text);
 		return ResponseEntity.ok().body(list);
